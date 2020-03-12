1622c7dcaa14dd4856371df3c4e1274e9efcde91
bergmaner <bergkacper@gmail.com>
2020-03-12 14:34:26 +0100

    connect with firebase and install packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = requie("firebase");
+require ("firebase/firestore");
+firebase.initializeApp({
+    apiKey: "AIzaSyA6Ccjz0pBXgQlerA7Tj7BnyzboTGsajOQ",
+    authDomain: "chat-app-1ecbc.firebaseapp.com",
+    databaseURL: "https://chat-app-1ecbc.firebaseio.com",
+    projectId: "chat-app-1ecbc",
+    storageBucket: "chat-app-1ecbc.appspot.com",
+    messagingSenderId: "933134151767",
+    appId: "1:933134151767:web:68b14b42369a741ff03312",
+    measurementId: "G-12BCPKQMZP"
+ 
+});
+ReactDOM.render(<div >HELOODOUD </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
