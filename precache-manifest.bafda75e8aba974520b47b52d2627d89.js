9fbe4a2f72c39655b5aead9a4494d2b61648b63e
bergmaner <bergkacper@gmail.com>
2020-03-24 14:53:03 +0100

    Updates

---
+++ precache-manifest.bafda75e8aba974520b47b52d2627d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b60407a2a87fe5a26b8a7317119379d",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "486ede2c44a6b40bae8a",
+    "url": "/chatapp/static/css/main.94a6b8b8.chunk.css"
+  },
+  {
+    "revision": "7453e999fd689d86f616",
+    "url": "/chatapp/static/js/2.0eee819f.chunk.js"
+  },
+  {
+    "revision": "a0c343326efedf2c357a1e9902399f0a",
+    "url": "/chatapp/static/js/2.0eee819f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486ede2c44a6b40bae8a",
+    "url": "/chatapp/static/js/main.db71e309.chunk.js"
+  },
+  {
+    "revision": "c5109094fa08c2172f4e",
+    "url": "/chatapp/static/js/runtime-main.8e94787f.js"
+  }
+]);
